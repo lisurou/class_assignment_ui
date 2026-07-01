f09b40d906b7f6507a74ca0d308bd6c5ae5e2937
guijie060304-pixel <guijie060304@gmail.com>
2026-07-01 16:12:38 +0800

    图片功能和作业提交开始时间

--- src/components/CourseAssignments.vue
+++ src/components/CourseAssignments.vue
@@ -211,6 +211,10 @@ const releaseDeadlineModel = computed({
   set: (value) => emit('update:releaseDeadline', value),
 });
 
+const disablePastDate = (time) => {
+  return time.getTime() < Date.now() - 86400000;
+};
+
 const releaseAllowFormatModel = computed({
   get: () => props.releaseAllowFormat,
   set: (value) => emit('update:releaseAllowFormat', value),
@@ -738,6 +742,7 @@ const updateDraftScore = (assignmentDetail, value) => {
                   format="YYYY-MM-DD HH:mm"
                   value-format="YYYY-MM-DD HH:mm"
                   :readonly="releasePublishTimeReadonly"
+                  :disabled-date="disablePastDate"
                   class="custom-date-picker"
                 />
               </div>
